dcc3c8572a300fc20fdc5f7f7d192546e33e3fb6
Delcy Villalba <delcyadaliz1@gmail.com>
2025-12-05 18:00:49 -0300

    Actualizar el punto final de inicio de sesión para el nuevo backend

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,15 +2,18 @@ const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const loginApi = async ({ email, password }) => {
   if (!email || !password) throw new Error("Ingresa email y contraseña");
-  const res = await fetch(baseURL + "/auth/login", {
+
+  const res = await fetch(${baseURL}/auth/login, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
+
+  const data = await res.json();
+
   if (!res.ok) {
-    const data = await res.json().catch(() => ({}));
     throw new Error(data.msg || "Credenciales inválidas");
   }
-  const user = await res.json();
-  return { ...user, token: "fake.jwt.token" };
+
+  return data;
 };
